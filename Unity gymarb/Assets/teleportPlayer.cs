86f7bd3fcb35179cb2917d1813607d685f436e80
xXminimannenXx <anton.finn@elev.ga.klaragymnasium.se>
2024-12-11 11:07:41 +0100

    teleportör fixad ez gg

---
+++ Unity gymarb/Assets/teleportPlayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleportPlayer : MonoBehaviour
+{
+  public Transform teleportTarget;
+  public GameObject player;
+
+  void OnTriggerEnter(Collider other)
+  {
+
+player.transform.position = teleportTarget.transform.position;
+
+  }
+
+
+
+}
